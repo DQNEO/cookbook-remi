b44a10e804e7056550e010737ffa64c41bce4943
DQNEO <dqneoo@gmail.com>
2014-03-13 00:47:18 +0900

    integrate recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,19 @@
-#
-# Cookbook Name:: yum-remi-php55
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# add remi repository
+yum_repository 'remi' do
+  description 'Les RPM de Remi - Repository'
+  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/remi/mirror'
+  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
+  fastestmirror_enabled true
+  enabled false
+  action :create
+end
 
-include_recipe "cookbook-remi::remi"
-include_recipe "cookbook-remi::remi-php55"
-
+# add remi-php55 repository
+yum_repository 'remi-php55' do
+  description 'Les RPM de Remi PHP55 - Repository'
+  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php55/mirror'
+  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
+  fastestmirror_enabled true
+  enabled false
+  action :create
+end
