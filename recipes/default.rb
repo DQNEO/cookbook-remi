36cc9aa43f5dff1464225eb7655511ff97d8d892
DQNEO <dqneoo@gmail.com>
2014-03-13 00:29:24 +0900

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: yum-remi-php55
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "cookbook-remi::remi"
+include_recipe "cookbook-remi::remi-php55"
+
