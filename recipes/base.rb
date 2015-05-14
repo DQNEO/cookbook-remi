d99e91fc93e36087a6ebd17516a171a57a0c1f45
DQNEO <dqneoo@gmail.com>
2015-05-14 18:54:35 +0900

    fix to work on CentOS7

--- recipes/base.rb
+++ recipes/base.rb
@@ -3,7 +3,13 @@ include_recipe "yum-epel"
 
 yum_repository 'remi' do
   description 'Les RPM de Remi - Repository'
-  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/remi/mirror'
+
+  if platform_family?('rhel') && node['platform_version'].to_i == 7 then
+    mirrorlist 'http://rpms.famillecollet.com/enterprise/7/remi/mirror'
+  elsif platform_family?('rhel') && node['platform_version'].to_i == 6 then
+    mirrorlist 'http://rpms.famillecollet.com/enterprise/6/remi/mirror'
+  end
+
   gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
   fastestmirror_enabled true
   enabled false
