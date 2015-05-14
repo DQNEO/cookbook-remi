586b65d8698da20bd18abe318104326db5b3efd8
DQNEO <dqneoo@gmail.com>
2015-05-14 20:56:56 +0900

    bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'remi'
 maintainer       'DQNEO'
 description      'Installs/Configures yum remi and/or remi-php55, remi-php56'
-version          '1.0.0'
+version          '1.1.0'
 
 recipe 'remi', 'Installs/Configures yum remi and/or remi-php55, remi-php56'
 
