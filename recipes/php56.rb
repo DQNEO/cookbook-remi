60e344f0edc488f569c600986b8c59e1bfe9b731
DQNEO <dqneoo@gmail.com>
2015-05-14 18:55:29 +0900

    fix to work on Centos7

--- recipes/php56.rb
+++ recipes/php56.rb
@@ -3,7 +3,13 @@ include_recipe "remi::base"
 # add remi-php56 repository
 yum_repository 'remi-php56' do
   description 'Les RPM de Remi PHP56 - Repository'
-  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php56/mirror'
+  if platform_family?('rhel') && node['platform_version'].to_i == 7 then
+    mirrorlist 'http://rpms.famillecollet.com/enterprise/7/php56/mirror'
+  elsif platform_family?('rhel') && node['platform_version'].to_i == 6 then
+    mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php56/mirror'
+  end
+
+
   gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
   fastestmirror_enabled true
   enabled false
